20c0f1d10921ba07a184a1aeac16122492734c15
Maxim <polyakovmaxim05@gmail.com>
2023-01-31 14:25:15 -0500

    first commit

---
+++ address/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Address(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+
